6d6eed2375b936794360eff84a124d36ba3c0d7a
haeshinholdings <dev.haeshinholdings@gmail.com>
2019-08-27 14:22:16 +0900

    기본 틀

--- components/main/ApplicationOfAlibaba.vue
+++ components/main/ApplicationOfAlibaba.vue
@@ -2,13 +2,25 @@
     <div class="hs-application">
         <div class="columns">
             <div class="column is-offset-one-quarter">
-                tet
+                <figure class="iamge">
+                    <img src="~assets/img/index70_2.png" />
+                </figure>
+            </div>
+            <div class="column">
+                <b-button @click="gotoRegister" size="is-large">가입문의</b-button>
             </div>
         </div>
         <div class="columns">
-            <div class="column is-offset-one-quarter">
-                map
-            </div>
         </div>
     </div>
 </template>
+
+<script>
+export default {
+  methods:{
+        gotoRegister(){
+            window.open('https://forms.gle/uX1gfGShtdmv26kt8');
+        }
+  }
+}
+</script>
